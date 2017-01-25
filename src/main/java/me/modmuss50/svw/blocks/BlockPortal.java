b92b5344a780ce9913cdfd95d2a88f85c2587133
modmuss50 <modmuss50@gmail.com>
2017-01-25 15:52:18 +0000

    Prevent the portal being broken in the void world, removes a dupe bug.
    (cherry picked from commit 4c7439e)

--- src/main/java/me/modmuss50/svw/blocks/BlockPortal.java
+++ src/main/java/me/modmuss50/svw/blocks/BlockPortal.java
@@ -46,4 +46,12 @@ public class BlockPortal extends Block {
 		}
 		return super.onBlockActivated(worldIn, pos, state, playerIn, hand, side, hitX, hitY, hitZ);
 	}
+
+	@Override
+	public float getBlockHardness(IBlockState blockState, World worldIn, BlockPos pos) {
+		if(worldIn.provider.getDimension() == Config.dimID){
+			return 1000F;
+		}
+		return super.getBlockHardness(blockState, worldIn, pos);
+	}
 }
